3e38ba5f8e0528b3bb9c14596febeed72a701ad1
sysstas <sysstas@gmail.com>
2018-10-17 13:30:43 +0300

    orders refactoring

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -12,16 +12,7 @@ const helper = new JwtHelperService();
 @Injectable()
 export class ApiService {
 
-  // masters = [];
   users = [];
-  orders = [];
-  // masterRating = [
-  //   {mark: 1},
-  //   {mark: 2},
-  //   {mark: 3},
-  //   {mark: 4},
-  //   {mark: 5}
-  // ];
   initialUserData = new UserRegInfo('', '', '', '', 0);
   currentUser = {};
   newOrderInformation;
@@ -32,7 +23,7 @@ export class ApiService {
   IsLoggedIn = false;
 
 //  addr = "https://blooming-ocean-36906.herokuapp.com"
-  addr = ' https://4b225c15.ngrok.io';
+  addr = 'https://b6d91318.ngrok.io';
   // addr = "http://localhost:5000"
   TOKEN_KEY = 'token';
 
@@ -43,45 +34,6 @@ export class ApiService {
   get token() {
     return localStorage.getItem(this.TOKEN_KEY);
   }
-
-
-
-  /////// CRUD Masters /////////////////////////////// asdasd
-  // getMasters() {
-  //   this.http.get<any>(this.addr + '/masters').subscribe( res => {
-  //     this.masters = res;
-  //     console.log('get masters:', res);
-  //   });
-  // }
-
-  // addMaster(newMaster) {
-  //   this.http.post(this.addr + '/masters', newMaster).subscribe( res => {
-  //     if (res) {
-  //       this.getMasters();
-  //       this.openSnackBar('Master succesfully saved');
-  //     }
-  //   });
-  // }
-
-  // editMaster(data) {
-  //   this.http.put(this.addr + '/masters/' + data.id, data).subscribe( res => {
-  //     if (res) {
-  //       console.log('API Master edit data: ', data);
-  //       this.getMasters();
-  //       this.openSnackBar('Master succesfully saved');
-  //     }
-  //   });
-  // }
-
-  // deleteMaster(data) {
-  //   this.http.delete(this.addr + '/masters/' + data.id).subscribe(res => {
-  //     this.getMasters();
-  //     this.openSnackBar('Master succesfully deleted');
-  //   });
-  // }
-  //////////////////////////////////////
-
-  /////// CRUD Users ///////////////////////////////
 
   getInitialRegisterData(data) {
     console.log('API.getInitialRegisterData runs, id - ', data.id);
@@ -104,13 +56,11 @@ export class ApiService {
     this.router.navigate(['/']);
   }
 
-
   getClients() {
     this.http.get<any>(this.addr + '/users').subscribe( res => {
       this.users = res;
     });
   }
-
 
   addClient(query) {
     this.http.post<any>(this.addr + '/users', query).subscribe(res => {{
@@ -139,36 +89,6 @@ export class ApiService {
       this.openSnackBar('Client succesfully deleted');
     });
   }
-  //////////////////////////////////////
-
-  /////// CRUD Orders //////////////////////
-  getOrders() {
-    return this.http.get<any>(this.addr + '/orders').subscribe(res => {
-      if (res) {
-        // console.log('API TEST', res)
-        res.map( function(x) {
-          x.city = x.city.cityName;
-          x.master = x.master.masterName;
-          x.userName = x.user.userName;
-          x.userEmail = x.user.userEmail;
-          return x;
-        });
-        // console.log('API TEST arr', arr)
-        this.orders = res;
-        console.log('API get orders: ', this.orders);
-      }
-    });
-  }
-
-  getOrdersAfterChange(): Observable<any> {
-    return this.http.get(this.addr + '/orders');
-  }
-
-  deleteOrder(id): Observable<any> {
-    return this.http.delete(this.addr + '/orders/' + id);
-  }
-
-
 
   createOrder(newOrder) {
     console.log('API send order', newOrder);
@@ -183,81 +103,11 @@ export class ApiService {
     }});
   }
 
-  editOrder(data) {
-    const orderInfo = {
-      cityID: data.cityID,
-      masterID: data.masterID,
-      clientID: data.clientID,
-      date: data.date,
-      time: data.time,
-      duration: data.duration
-    };
-    console.log('send order ', data);
-    this.http.put(this.addr + '/orders/' + data.id, orderInfo).subscribe(res => {{
-        console.log(res);
-        this.getOrders();
-    }});
-  }
-  /////////////////////////////////////
-
-  ////// App logic ///////////////////
-
   getFreeMasters(query) {
      this.http.post<any>(this.addr + '/freemasters', query).subscribe( res => {
        this.arr = res;
     });
   }
-
-
-  // getMastersShedule(query) {
-  //   // console.log('schedule query: ', query)
-  //   this.http.post<any>(this.addr + '/schedule', query).subscribe( res => {
-  //     // this.schedule = res.json()
-  //     // console.log("received schedule data: ",res.json())
-  //     // this.schedule = [{name:"Alice",hours:['qwe','qwe']}]
-  //     const temp = res;
-  //     temp.forEach(element => {
-  //       element.hours = [];
-  //       for (let i = 0; i < element.duration; i++) {
-  //         element.hours[element.time - 8 + i] = element.ID;
-  //       }
-  //       // console.log(element)
-  //     });
-  //     this.schedule = temp;
-  //     temp.forEach(element => {
-
-  //     });
-  //     // console.log(temp)
-  //     // {ID: 15, masterName: "Nastya", time: 8, duration: 3}
-  //     console.log('received schedule temp: ', temp);
-  //     // /// making appropriate array of hours when master is busy
-  //     // this.schedule.forEach((master, index, array) => {
-  //     //   master.hours = []
-  //     //   let arr = master.busy[0].time
-  //     //   let length = arr.length
-  //     //   for (let index = 0; index < length; index++) {
-  //     //     const element = arr[index];
-  //     //     let i = element - 8
-  //     //     master.hours[i] = "Busy"
-  //     //   }
-  //     // })
-  //   });
-  // }
-
-  // updateMasterSchedule(orderInfo) {
-  //   this.http.post(this.addr + '/order', orderInfo).subscribe(res => {
-  //     if (res) {
-  //       console.log('master schedule updated');
-  //       this.openSnackBar('You successfuly order a master');
-  //       // refreshing masters array after adding new city
-  //       this.getMasters();
-  //       this.getOrders();
-  //     }
-  //   });
-  // }
-
-
-/////////////////// LOGIN
 
   Auth(login, password, googleToken): void {
     const querry = { login, password, googleToken};

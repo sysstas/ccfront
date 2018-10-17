34c3e9a66a55f0e4e8206a674940565a114dd51d
sysstas <sysstas@gmail.com>
2018-10-17 13:54:17 +0300

    slight refactoring and bugfixing

--- src/app/client/user-register/user-register.component.ts
+++ src/app/client/user-register/user-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../api.service';
 import { FormControl, Validators } from '@angular/forms';
 
@@ -9,30 +9,24 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent implements OnInit {
-  // Form validation part
-  email = new FormControl('', [Validators.required, Validators.email])
-  name = new FormControl('', [Validators.required])
-  password = new FormControl('', [Validators.required])
-  passwordconfirm = new FormControl('', [Validators.required])
+  userData = this.api.initialUserData;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  name = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+  passwordconfirm = new FormControl('', [Validators.required]);
 
-
-
-
-
-  constructor( public api: ApiService, private _route: ActivatedRoute) { 
+  constructor( public api: ApiService, private _route: ActivatedRoute) {
     // console.log("route parameters - id: ", this._route.snapshot.paramMap.get('id'))
   }
-  
+
   ngOnInit() {
-    let id = this._route.snapshot.paramMap.get('id')
-    console.log(id)
-    this.api.getInitialRegisterData({id:id})
-    
+    const id = this._route.snapshot.paramMap.get('id');
+    // console.log(id);
+    this.api.getInitialRegisterData({id: id});
   }
-  
+
   register() {
-    console.log("USER_REGISTER",this.api.initialUserData)
-    this.api.postRegisteredUserData()
+    // console.log('USER_REGISTER', this.api.initialUserData);
+    this.api.postRegisteredUserData();
   }
-  userData = this.api.initialUserData
 }

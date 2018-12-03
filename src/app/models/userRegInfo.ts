3ae58efb34bd76f658bb3ed468819066b7231b08
sysstas <sysstas@gmail.com>
2018-12-03 12:51:32 +0200

    refactored

--- src/app/models/userRegInfo.ts
+++
@@ -1,9 +0,0 @@
-export class UserRegInfo {
-    constructor(
-        public userName: string,
-        public userEmail: string,
-        public password: string,
-        public passwordConfirm: string,
-        public isRegistered: number,
-    ) {}
-}
